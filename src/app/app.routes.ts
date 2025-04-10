bf45cca28436119475550290bc21ebdd2fe99dfc
su4g <suag725@163.com>
2025-04-10 15:39:48 +0800

    fix : build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,5 +14,6 @@ export const routes: Routes = [
     {
         path: 'screen/:id',
         component: ScreenComponent
-    }
+    },
+    { path: '', redirectTo: 'control', pathMatch: 'full' },
 ];
